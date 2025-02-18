c8d7ceb69e15def18af0fc79cbac7eeb16d31132
George Asiedu <asiedug41@gmail.com>
2025-02-18 12:14:34 +0000

    store state persisted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NgToastModule, ToasterPosition} from 'ng-angular-popup';
+import { appActions } from './app-store/app.actions';
+import { constants } from './utils/constants';
+import { Store } from '@ngrx/store';
+import {selectAppStoreFeature} from './app-store/app.selectors';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +12,25 @@ import {NgToastModule, ToasterPosition} from 'ng-angular-popup';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   public toastPosition = ToasterPosition.TOP_RIGHT;
+
+  private store= inject(Store);
+  private state = this.store.selectSignal(selectAppStoreFeature);
+  private key: string = constants.storageKey;
+
+  @HostListener('window:beforeunload', ['$event'])
+  public beforeunloadHandler(): void {
+    const storageValue = JSON.stringify(this.state());
+    localStorage.setItem(this.key, storageValue);
+  }
+
+  public ngOnInit() {
+    const persistState = localStorage.getItem(this.key);
+    if (persistState) {
+      const storeData = JSON.parse(persistState);
+      this.store.dispatch(appActions.getAuthData(storeData));
+    }
+    localStorage.removeItem(this.key);
+  }
 }
