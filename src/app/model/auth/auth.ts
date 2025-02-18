cf7aa04f5c22b35adf1c0033468587f829e74f3a
George Asiedu <asiedug41@gmail.com>
2025-02-18 12:18:28 +0000

    auth interfaces created

---
+++ src/app/model/auth/auth.ts
@@ -0,0 +1,62 @@
+export interface Register {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface Signin {
+  email: string;
+  password: string;
+}
+
+export interface VerifyAccount {
+  code: string;
+}
+
+export interface RegisterResponse {
+  message: "Success",
+  data: {
+    user: {
+      id: 1,
+      name: string,
+      email: string,
+      role: string,
+      isVerified: boolean,
+      programme: null,
+      dob: null,
+      phone: null,
+      address: null,
+      registered: boolean
+    },
+    token: string
+  }
+}
+
+export interface SigninResponse {
+  message: "Success",
+  data: {
+    accessToken: string;
+    refreshToken: string;
+    user: {
+      id: 1,
+      name: string,
+      email: string,
+      role: string,
+      isVerified: boolean,
+      programme: null,
+      dob: null,
+      phone: null,
+      address: null,
+      registered: boolean
+    }
+  }
+}
+
+export interface SuccessResponse {
+  message: string;
+}
+
+export enum Roles {
+  Admin = 'Admin',
+  Employee = 'Student',
+}
