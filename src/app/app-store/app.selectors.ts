a8bebfe69fc18f5cc9b1a9c9d80535a023ad187b
George Asiedu <asiedug41@gmail.com>
2025-02-18 15:16:30 +0000

    deleted

--- src/app/app-store/app.selectors.ts
+++
@@ -1,4 +0,0 @@
-import { createFeatureSelector } from '@ngrx/store';
-import {AppStoreState} from './app.state';
-
-export const selectAppStoreFeature = createFeatureSelector<AppStoreState>('AppStore');
--- src/app/app-store/app.state.ts
+++
@@ -1,7 +0,0 @@
-import { SigninResponse } from "../model/auth/auth";
-
-export interface AppStoreState {
-  isLoading: boolean;
-  error: string | null;
-  user: SigninResponse | null;
-}
