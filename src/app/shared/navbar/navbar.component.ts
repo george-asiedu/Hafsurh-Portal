31db4c72c642c649f4b2299698e3a1266e2c54b9
George Asiedu <asiedug41@gmail.com>
2025-02-18 15:18:45 +0000

    navbar profile implemented

---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,39 @@
+import {Component, OnInit} from '@angular/core';
+import { authActions } from '../../auth/store/auth.actions';
+import {Store} from '@ngrx/store';
+import {AuthState} from '../../auth/store/auth.state';
+import {selectUser} from '../../auth/store/auth.selectors';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: false,
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent implements OnInit {
+  public isDropdownOpen: boolean = false;
+  public name: string = '';
+
+  public constructor(private store: Store<AuthState>) {}
+
+  public ngOnInit() {
+    const currentUser = this.store.selectSignal(selectUser);
+    const user = currentUser();
+    if (user) {
+      this.name = user.name;
+    }
+  }
+
+  public toggleDropdown(): void {
+    this.isDropdownOpen = !this.isDropdownOpen
+  }
+
+  public profile(): void {
+    this.isDropdownOpen = false
+  }
+
+  public logout(): void {
+    authActions.logout();
+    this.isDropdownOpen = false
+  }
+}
