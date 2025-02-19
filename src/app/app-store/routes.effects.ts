434980d9b3e81a97dec0f25c4930fbf564ebee52
George Asiedu <asiedug41@gmail.com>
2025-02-19 13:51:26 +0000

    route path defined for user roles

--- src/app/app-store/routes.effects.ts
+++ src/app/app-store/routes.effects.ts
@@ -3,6 +3,7 @@ import {Actions, createEffect, ofType} from '@ngrx/effects';
 import {Router} from '@angular/router';
 import {tap} from 'rxjs';
 import { authActions } from '../auth/store/auth.actions';
+import {Roles} from '../model/auth/auth';
 
 export const registerAccountRouteEffects = createEffect(
   (actions$ = inject(Actions), route = inject(Router)) => {
@@ -28,7 +29,14 @@ export const signinRouteEffects = createEffect(
   (actions$ = inject(Actions), route = inject(Router)) => {
     return actions$.pipe(
       ofType(authActions.signinSuccess),
-      tap(() => route.navigateByUrl('/dashboard'))
+      tap((response) => {
+        if(response.data.data.user.role == Roles.Admin) {
+          route.navigateByUrl('/admin-dashboard')
+        }
+        else if (response.data.data.user.role == Roles.Student) {
+          route.navigateByUrl('/student-dashboard')
+        }
+      })
     );
   },
   { functional: true, dispatch: false }
