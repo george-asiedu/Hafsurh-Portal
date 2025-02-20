b6a5f4d34f9ce51898117a138311afe2262c67e4
George Asiedu <asiedug41@gmail.com>
2025-02-20 22:50:10 +0000

    admin service implemented

---
+++ src/app/admin/service/admin.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {AddCourse, CourseResponse, GetAllCourses, UpdateCourse} from '../../model/courses/courses';
+import {environment} from '../../../environments/environment';
+import {SuccessResponse} from '../../model/auth/auth';
+import {GetAllUsers, Profile, UpdateBio} from '../../model/users/users';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+  private appUrl: string = environment.productionEnvironment;
+
+  constructor(private http: HttpClient) {}
+
+  public getAllCourses() {
+    return this.http.get<GetAllCourses>(`${this.appUrl}courses/all-courses`);
+  }
+
+  public addCourse(course: AddCourse) {
+    return this.http.post<CourseResponse>(`${this.appUrl}courses/add-course`, course);
+  }
+
+  public updateCourse(id: string, course: UpdateCourse) {
+    return this.http.patch<CourseResponse>(`${this.appUrl}courses/update-course/${id}`, course);
+  }
+
+  public registerCourse(courseId: string) {
+    return this.http.post<SuccessResponse>(`${this.appUrl}courses/register/${courseId}`, {});
+  }
+
+  public deleteCourse(id: string) {
+    return this.http.delete<SuccessResponse>(`${this.appUrl}courses/${id}`);
+  }
+
+  public getUserProfile(id: string) {
+    return this.http.get<Profile>(`${this.appUrl}users/profile/${id}`);
+  }
+
+  public getAllUsers() {
+    return this.http.get<GetAllUsers>(`${this.appUrl}users/all-users`);
+  }
+
+  public updateBioData(id: string, user: UpdateBio) {
+    return this.http.patch<Profile>(`${this.appUrl}users/bio-data/${id}`, user);
+  }
+}
