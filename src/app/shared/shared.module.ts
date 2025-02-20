7f9cfa55d809c1cc78be061597fb38735644b5ee
George Asiedu <asiedug41@gmail.com>
2025-02-20 22:32:28 +0000

    components added to module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, DatePipe, NgClass, NgFor, NgIf} from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import {RouterLink} from '@angular/router';
+import { CoursesTableComponent } from './courses-table/courses-table.component';
+import {NgxSpinnerModule} from 'ngx-spinner';
+import { EditCourseModalComponent } from './edit-course-modal/edit-course-modal.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { AddCourseModalComponent } from './add-course-modal/add-course-modal.component';
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    CoursesTableComponent,
+    EditCourseModalComponent,
+    AddCourseModalComponent
   ],
   imports: [
     CommonModule,
-    RouterLink
+    RouterLink,
+    NgFor,
+    NgIf,
+    NgClass,
+    DatePipe,
+    NgxSpinnerModule,
+    ReactiveFormsModule
   ],
-  exports: [NavbarComponent]
+  exports: [
+    NavbarComponent,
+    CoursesTableComponent,
+    AddCourseModalComponent,
+    CoursesTableComponent
+  ]
 })
 export class SharedModule { }
