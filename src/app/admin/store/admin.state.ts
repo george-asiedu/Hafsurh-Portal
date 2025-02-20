ece526297b9ce01d18b5dd1baecf3ba0c4e2a575
George Asiedu <asiedug41@gmail.com>
2025-02-20 22:48:13 +0000

    admin state defined

---
+++ src/app/admin/store/admin.state.ts
@@ -0,0 +1,18 @@
+import {CourseResponse, GetAllCourses} from '../../model/courses/courses';
+import {SuccessResponse} from '../../model/auth/auth';
+import {HttpErrorResponse} from '@angular/common/http';
+import {GetAllUsers, Profile} from '../../model/users/users';
+
+export interface AdminState {
+  isLoading: boolean;
+  error: HttpErrorResponse | null;
+  data: {type: 'addCourse', response: CourseResponse} |
+    {type: 'updateCourse', response: CourseResponse} |
+    {type: 'getCourses', response: GetAllCourses} |
+    null;
+  user: {type: 'getUserProfile', response: Profile} |
+    {type: 'updateBio', response: Profile} |
+    {type: 'getAllUsers', response: GetAllUsers} |
+    null;
+  message: SuccessResponse | null;
+}
