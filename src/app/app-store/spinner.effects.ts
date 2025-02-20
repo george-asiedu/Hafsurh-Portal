be8fa62aa43aa08e70fd1bde15b3569cf108f8d2
George Asiedu <asiedug41@gmail.com>
2025-02-20 22:47:48 +0000

    spinner effects updated

--- src/app/app-store/spinner.effects.ts
+++ src/app/app-store/spinner.effects.ts
@@ -3,14 +3,17 @@ import {Actions, createEffect, ofType } from '@ngrx/effects';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { tap } from 'rxjs';
 import {authActions} from '../auth/store/auth.actions';
+import {adminActions} from '../admin/store/admin.actions';
 
 export const showSpinnerEffects = createEffect(
   (actions$ = inject(Actions), spinner = inject(NgxSpinnerService)) => {
     return actions$.pipe(
       ofType(
-        authActions.registerAccount,
-        authActions.signin,
-        authActions.verifyAccount
+        authActions.registerAccount, authActions.signin,
+        authActions.verifyAccount, adminActions.updateUserBio,
+        adminActions.getUserProfile, adminActions.getAllUsers,
+        adminActions.addCourse, adminActions.deleteCourse,
+        adminActions.registerCourse, adminActions.updateCourse
       ),
       tap(() => spinner.show())
     );
@@ -22,10 +25,13 @@ export const hideSpinnerEffects = createEffect(
   (actions$ = inject(Actions), spinner = inject(NgxSpinnerService)) => {
     return actions$.pipe(
       ofType(
-        authActions.registerAccountSuccess,
-        authActions.signinSuccess,
-        authActions.authenticationFailure,
-        authActions.verifyAccountSuccess,
+        authActions.registerAccountSuccess, authActions.signinSuccess,
+        authActions.authenticationFailure, authActions.verifyAccountSuccess,
+        adminActions.getUserProfileSuccess, adminActions.getAllUsersSuccess,
+        adminActions.updateUserBioSuccess, adminActions.updateCourseSuccess,
+        adminActions.deleteCourseSuccess, adminActions.getAllCoursesSuccess,
+        adminActions.courseRegistrationSuccess, adminActions.updateCourseSuccess,
+        adminActions.userFailure, adminActions.courseFailure
       ),
       tap(() => spinner.hide())
     );
