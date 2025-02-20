e8e584227a539e84927027aab7fe2ec74f0104f0
George Asiedu <asiedug41@gmail.com>
2025-02-20 22:40:36 +0000

    users object interface defined

---
+++ src/app/model/users/users.ts
@@ -0,0 +1,29 @@
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  role: string;
+  isVerified: boolean;
+  programme: string | null;
+  dob: Date | null;
+  phone: string | null;
+  address: string | null;
+  registered: boolean
+}
+
+export interface Profile {
+  message: string;
+  data: User;
+}
+
+export interface GetAllUsers {
+  message: string;
+  data: User[];
+}
+
+export interface UpdateBio {
+  programme: string | null;
+  dob: Date | null;
+  phone: string | null;
+  address: string | null;
+}
