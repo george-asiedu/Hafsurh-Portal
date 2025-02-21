7cb17061f8f3ee4de993edad7c28a1e2cf9e0880
George Asiedu <asiedug41@gmail.com>
2025-02-21 09:05:50 +0000

    RefreshTokenResponse updated

--- src/app/auth/store/auth.state.ts
+++ src/app/auth/store/auth.state.ts
@@ -1,11 +1,11 @@
-import {RegisterResponse, SigninResponse, SuccessResponse} from '../../model/auth/auth';
+import {RefreshTokenResponse, RegisterResponse, SigninResponse, SuccessResponse} from '../../model/auth/auth';
 import {HttpErrorResponse} from '@angular/common/http';
 
 export interface AuthState {
   isLoading: boolean;
   data: { type: 'register'; response: RegisterResponse } |
     { type: 'signin'; response: SigninResponse } |
-    { type: 'refreshToken'; response: SigninResponse } | null;
+    { type: 'refreshToken'; response: RefreshTokenResponse } | null;
   message: SuccessResponse | null;
   error: HttpErrorResponse | null;
 }
