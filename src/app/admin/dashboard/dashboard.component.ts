cae94ae1a970187d21247657e956a493155db9c7
George Asiedu <asiedug41@gmail.com>
2025-02-22 23:30:24 +0000

    updated

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, computed, inject, OnInit} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {AdminState} from '../store/admin.state';
+import {selectUsersData} from '../store/admin.selector';
+import {selectUser} from '../../auth/store/auth.selectors';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,6 +10,18 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent  implements OnInit {
+  public today: Date = new Date();
+  public name: string = '';
+  private store = inject(Store<AdminState>);
+  public usersData = this.store.selectSignal(selectUsersData);
+  public count = computed(() => this.usersData().length);
 
+  public ngOnInit() {
+    const currentUser = this.store.selectSignal(selectUser);
+    const user = currentUser();
+    if (user) {
+      this.name = user.name;
+    }
+  }
 }
--- src/app/student/dashboard/dashboard.component.ts
+++ src/app/student/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, computed, inject, OnInit} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {AdminState} from '../../admin/store/admin.state';
+import {selectUsersData} from '../../admin/store/admin.selector';
+import {selectUser} from '../../auth/store/auth.selectors';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,6 +10,27 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  public today: Date = new Date();
+  public name: string = '';
+  public dob: string | null = 'N/A';
+  public address: string | null = 'N/A';
+  public phone: string | null = 'N/A';
+  public programme: string | null = 'N/A';
+  public email: string = '';
 
+  constructor(private store: Store) {}
+
+  public ngOnInit() {
+    const currentUser = this.store.selectSignal(selectUser);
+    const user = currentUser();
+    if (user) {
+      this.name = user.name;
+      this.email = user.email;
+      this.address = user.address;
+      this.phone = user.phone;
+      this.programme = user.programme;
+      this.dob = user.dob;
+    }
+  }
 }
