4a3b1eeff61d6ed18315d7b09aa1ac2b2020b6c3
George Asiedu <asiedug41@gmail.com>
2025-02-23 17:37:57 +0000

    NgxSpinnerModule imported

--- src/app/student/student.module.ts
+++ src/app/student/student.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { StudentRoutingModule } from './student-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import {SharedModule} from '../shared/shared.module';
+import {NgxSpinnerModule} from 'ngx-spinner';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import {SharedModule} from '../shared/shared.module';
   imports: [
     CommonModule,
     StudentRoutingModule,
-    SharedModule
+    SharedModule,
+    NgxSpinnerModule
   ]
 })
 export class StudentModule { }
