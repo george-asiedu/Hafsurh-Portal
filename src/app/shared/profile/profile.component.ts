5d208221aefd576729285ff0d0fac362acea7315
George Asiedu <asiedug41@gmail.com>
2025-02-23 17:38:57 +0000

    get profile action dispatched

---
+++ src/app/shared/profile/profile.component.ts
@@ -0,0 +1,39 @@
+import {Component, inject, OnInit} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {ActivatedRoute} from '@angular/router';
+import {adminActions} from '../../admin/store/admin.actions';
+import {selectLoading, selectUserProfile} from '../../admin/store/admin.selector';
+
+@Component({
+  selector: 'app-profile',
+  standalone: false,
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.scss'
+})
+export class ProfileComponent implements OnInit {
+  private store = inject(Store);
+  public profile = this.store.selectSignal(selectUserProfile);
+  public isLoading = this.store.selectSignal(selectLoading);
+  public isModalVisible: boolean = false;
+
+  constructor(private route: ActivatedRoute) {}
+
+  public ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.store.dispatch(adminActions.getUserProfile({id}));
+    }
+  }
+
+  showModal() {
+    this.isModalVisible = true;
+  }
+
+  cancelModal() {
+    this.isModalVisible = false;
+  }
+
+  confirmData() {
+    this.isModalVisible = false;
+  }
+}
