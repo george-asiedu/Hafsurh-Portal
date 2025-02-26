66ed8f9de9863d43d64a5383a828a2668c042ee6
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2025-02-26 13:05:50 +0000

    course id and register success message added

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -7,4 +7,7 @@ export const constants = {
   unauthorizedAccess: 'Unauthorized route access, please login',
   accessDenied: 'Access denied',
   storageKey: 'appStoreState',
+  courseId: '6b59f20f-f395-4189-b6e4-77cbb1f79ee0',
+  registerSuccess: 'Courses registered successfully',
+  updateBioSuccess: 'Bio updated successfully',
 };
