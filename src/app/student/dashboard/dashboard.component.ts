12fa99f320f773d1c7c78b2bf73cba600364ef49
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2025-02-26 13:07:00 +0000

    registerCourse method updated

--- src/app/student/dashboard/dashboard.component.ts
+++ src/app/student/dashboard/dashboard.component.ts
@@ -1,7 +1,13 @@
 import {Component,OnInit} from '@angular/core';
 import {Store} from '@ngrx/store';
-import {selectCourseId, selectUserProfile} from '../../admin/store/admin.selector';
+import {selectUserProfile} from '../../admin/store/admin.selector';
 import {adminActions} from '../../admin/store/admin.actions';
+import {selectUser} from '../../auth/store/auth.selectors';
+import {AdminService} from '../../admin/service/admin.service';
+import {constants} from '../../utils/constants';
+import {NgxSpinnerService} from 'ngx-spinner';
+import {NgToastService} from 'ng-angular-popup';
+import {HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
@@ -17,13 +23,23 @@ export class DashboardComponent implements OnInit {
   public phone: string | null = 'N/A';
   public programme: string | null = 'N/A';
   public email: string = '';
-  public courseId: string = '';
+  public id: string = '';
+  public courseId: string = constants.courseId;
+  public isModalVisible: boolean = false;
+  public loading: boolean = false;
 
-  constructor(private store: Store) {}
+  constructor(
+    private store: Store,
+    private adminService: AdminService,
+    private spinner: NgxSpinnerService,
+    private toast: NgToastService
+  ) {}
 
   public ngOnInit() {
     const currentUser = this.store.selectSignal(selectUserProfile);
     const user = currentUser();
+    const authInfo = this.store.selectSignal(selectUser)
+    const userId = authInfo();
     if (user) {
       this.name = user.name;
       this.email = user.email;
@@ -32,15 +48,38 @@ export class DashboardComponent implements OnInit {
       this.programme = user.programme;
       this.dob = user.dob;
     }
+    if (userId) {
+      this.id = userId.id;
+    }
+    this.store.dispatch(adminActions.getUserProfile({id: this.id}));
   }
 
   public registerCourse() {
-    const id = this.store.selectSignal(selectCourseId);
-    const activeId = id();
-    if (activeId) {
-      this.courseId = activeId;
-      console.log('course id: ', this.courseId);
-    }
-    this.store.dispatch(adminActions.registerCourse({courseId: this.courseId}));
+    this.loading = true;
+    this.spinner.show();
+    this.adminService.registerCourse(this.courseId).subscribe({
+      next: (response) => {
+        this.loading = false;
+        this.spinner.hide();
+        this.toast.success(constants.registerSuccess, response.message, constants.toastDuration);
+      },
+      error: (error: HttpErrorResponse) => {
+        this.loading = false;
+        this.spinner.hide();
+        this.toast.danger(constants.error, error.message, constants.toastDuration);
+      }
+    })
+  }
+
+  showModal() {
+    this.isModalVisible = true;
+  }
+
+  cancelModal() {
+    this.isModalVisible = false;
+  }
+
+  confirmData() {
+    this.isModalVisible = false;
   }
 }
